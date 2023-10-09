a3f8b40e0e1384cf8a67db743e06a825610adcdb
Parelp1 <fabian.march97@gmail.com>
2023-10-09 12:03:53 -0300

    Comienzo js

---
+++ clase 13/ejemplo01.js
@@ -0,0 +1,10 @@
+/*Declara variables del tipo string*/ 
+const nombre= "Fabián"
+const edad= 22;
+
+
+
+/*Haciendo un print*/
+
+document.write(nombre +" Edad: "+ edad);
+console.log(`Mi nombre es ${nombre} y tengo ${edad} años `) 
---
+++ clase 13/ejemplo02.js
@@ -0,0 +1,12 @@
+const nombre= prompt("ingrese su nombre", '');
+
+const edad= prompt("ingrese su edad",'');
+
+const num1= parseInt( prompt("Ingrese valor 1",''));
+
+const num2= parseInt( prompt("Ingrese valor 2",''));
+
+const suma= num1+num2
+
+console.log(`hola ${nombre} asi que tienes ${edad} años.`)
+console.log(suma);
